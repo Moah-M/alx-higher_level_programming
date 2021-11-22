2ff87abfbcf28449cb26f839e20fb8f7aff04a53
Moah-M <menelik.solomon@yahoo.com>
2021-11-22 20:05:58 +0300

    8th

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     x = len(sentence)
     y = sentence[0]
     if x == 0:
-        new_tuple = (x, "None")
+        new_tuple = (x, None)
     else:
         new_tuple = (x, y)
     return new_tuple

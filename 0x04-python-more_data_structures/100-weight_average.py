1d96ab1a079feaa1b2bf6fb84435f0000e3506e1
Moah-M <menelik.solomon@yahoo.com>
2021-11-24 20:21:25 +0300

    Seal

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    n = 0
+    m = 0
+
+    for val in my_list:
+        n += val[0] * val[1]
+        o += val[1]
+i
+    return (n / o)

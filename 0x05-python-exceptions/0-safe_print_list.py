2b0b58b9447a3b37b43595a9c9b88c19eaba8011
Moah-M <menelik.solomon@yahoo.com>
2021-12-06 21:21:00 +0300

    Please

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
 
-    for i in range(x):
+    for y in range(x):
         try:
-            print("{:d}".format(my_list[i]), end='')
+            print("{:d}".format(my_list[y]), end='')
         except:
             break
         else:
